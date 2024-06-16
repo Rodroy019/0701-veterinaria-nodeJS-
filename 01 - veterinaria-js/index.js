ebad6c97e70c52a2591eba73480e3ed82a8ecbe7
Rodrigo Rivas <rodroy019@hotmail.com>
2024-06-15 22:36:11 -0500

    add: se agrega codigo a json y index

---
+++ 01 - veterinaria-js/index.js
@@ -0,0 +1,10 @@
+const { registrar, leer } = require('./operaciones');
+const [operacion, nombre, edad, tipo, color, enfermedad] = process.argv.slice(2);
+
+if (operacion === 'registrar') {
+    registrar(nombre, edad, tipo, color, enfermedad);
+} else if (operacion === 'leer') {
+    leer();
+} else {
+    console.log('Operación no Encontrada. Recuerda usar "registrar" o "leer".');
+}
